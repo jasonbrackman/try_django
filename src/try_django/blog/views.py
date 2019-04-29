88b03f83e6b6990b282fbad82a8f40e466b47ff0
jasonbrackman <brackman@gmail.com>
2019-04-29 16:28:20 +0100

    Reorganized the directory structure to reflect a more compartmentalized app structure.  Templates and URLs now reside (mostly) with the app itself instead of mixed with the general admin structure.

--- src/try_django/blog/views.py
+++ src/try_django/blog/views.py
@@ -11,14 +11,14 @@ def blog_post_list_view(request):
     # could list out or search for objects
 
     qs = BlogPost.objects.all()
-    template_name = "blog_post_list.html"
+    template_name = "blog/list.html"
     context = {"object_list": qs}
     return render(request, template_name, context)
 
 
 def blog_post_create_view(request):
     # create a blog post using a form
-    template_name = "blog_post_create.html"
+    template_name = "blog/create.html"
     context = {"form": None}
     return render(request, template_name, context)
 
@@ -27,20 +27,20 @@ def blog_post_detail_view(request, slug):
     # 1 object -> detail view
     # query_set = BlogPost.objects.filter(slug=slug)
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_post_detail.html"
+    template_name = "blog/detail.html"
     context = {"object": obj}
     return render(request, template_name, context)
 
 
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_post_update.html"
+    template_name = "blog/update.html"
     context = {"object": obj, "form": None}
     return render(request, template_name, context)
 
 
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
-    template_name = "blog_post_delete.html"
+    template_name = "blog/delete.html"
     context = {"object": obj, "form": None}
     return render(request, template_name, context)
