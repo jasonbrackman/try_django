75e8d07836eea90e5ba349dec5c84a855b565c17
jasonbrackman <brackman@gmail.com>
2019-05-01 13:33:24 +0100

    adding direct links to the blog posts on page view.

--- src/try_django/blog/models.py
+++ src/try_django/blog/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 User = settings.AUTH_USER_MODEL
 
+
 # Create your models here.
 class BlogPost(models.Model):
 
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
-
     title = models.CharField(max_length=128)
     slug = models.SlugField(unique=True)
     content = models.TextField(null=True, blank=True)
+
+    def get_absolute_url(self):
+        return f"/blog/{self.slug}"
+
+    def get_edit_url(self):
+        return f"/blog/{self.slug}/edit"
+
--- src/try_django/blog/urls.py
+++ src/try_django/blog/urls.py
@@ -10,6 +10,6 @@ from .views import (
 urlpatterns = [
     path('', blog_post_list_view),
     path('<str:slug>/', blog_post_detail_view),
-    path('<str:slug>/update/', blog_post_update_view),
+    path('<str:slug>/edit/', blog_post_update_view),  # uses the 'update.html'
     path('<str:slug>/delete/', blog_post_delete_view),
 ]
