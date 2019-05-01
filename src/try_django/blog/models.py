9ad3375ebdbe1975d6be8759571cad2a1c4ad477
jasonbrackman <brackman@gmail.com>
2019-05-01 12:19:52 +0100

    fix: syntax error: removed brackets from the end of a var

--- src/try_django/blog/models.py
+++ src/try_django/blog/models.py
@@ -6,7 +6,7 @@ User = settings.AUTH_USER_MODEL
 # Create your models here.
 class BlogPost(models.Model):
 
-    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL())
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
 
     title = models.CharField(max_length=128)
     slug = models.SlugField(unique=True)
